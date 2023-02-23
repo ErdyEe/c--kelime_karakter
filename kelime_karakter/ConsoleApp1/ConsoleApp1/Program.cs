61eeae81c33c285f0d5e8a29af3d5b8597d52664
ErdyEe <124555313+ErdyEe@users.noreply.github.com>
2023-02-24 01:50:53 +0300

    kullanı tarafında girilen metnin karakterini ve kelimesini sayan program
    ...

---
+++ kelime_karakter/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿//Kullanıcı tarafından girilen hangi boyuttaki bir metin için içerisinde ki kelime sayısını ve karakter sayısını bulan kodu yazınız.
+//Güncelleme: Eğer birden fazla bosluk varsa!!!
+
+//1---------------------------------------------------------------------------
+
+string kelime;
+int kelimeSayisi = 0;
+Console.WriteLine("yazınız");
+kelime = Console.ReadLine();
+
+if (kelime[0] == ' ' && kelime[1] == ' ') kelimeSayisi--;
+
+for (int i = 0; i < kelime.Length; i++)
+{
+
+    if (kelime[i] == ' ')
+    {
+        kelimeSayisi++;
+        if (kelime[i] == ' ' && kelime[i + 1] == ' ')
+        {
+            kelimeSayisi--;
+        }
+    }
+}
+Console.WriteLine($"{kelimeSayisi + 1} kelime \n karakter sayısı : {kelime.Length}");
+
+
+
+//2--------------------------------------------------------
+
+//string text =Console.ReadLine();
+//int kelimeSayisi = 0;
+//int sayac = 0;
+
+//for (int i = 0; i < text.Length; i++)
+//{
+//	if (text[i]==' ')
+//	{
+//		if (sayac>0)
+//		{
+//			kelimeSayisi++;
+//			sayac = 0;
+//		}
+//		else { sayac++; }
+//	}
+//}
+//Console.WriteLine($"{kelimeSayisi + 1} kelime \n karakter sayısı : {text.Length}");
+
+
